58e82fcd00866fffed534ab803ac613ac73dccdf
7007singh <shw12021982@gmail.com>
2023-01-21 22:20:07 +0530

    [Shweta] Modified: Calculate Wages till a condition of total working hours or days is reached for a month

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -9,35 +9,38 @@ namespace EmployeeWageComputation
             const int FULL_TIME = 1;
             const int PART_TIME = 2;
             const int MAX_WORKING_DAYS = 20;
+            const int MAX_WORKING_HRS = 100;
             const int EMP_RATE_PER_HR = 20;
-            int empHrs = 0, empWage = 0, day = 1, totalWage = 0;
+            int empHrs = 0, empWage = 0, day = 1, totalWage = 0, totalHrs = 0;
 
             Console.WriteLine("Welcome to EmployeeWage Computation");
 
             //UC5 - CalculateWageForAMonth
             Random random = new Random();
-            for(day = 1; day <= MAX_WORKING_DAYS; day++)
+            while(day <= MAX_WORKING_DAYS && totalHrs <= MAX_WORKING_HRS)
             {
                 int empAttendance = random.Next(0, 3);
                 switch (empAttendance)
                 {
                     case FULL_TIME:
-                        Console.WriteLine("Full Time Employee is Present");
+                        Console.WriteLine("\nFull Time Employee is Present");
                         empHrs = 8;
                         break;
                     case PART_TIME:
-                        Console.WriteLine("Paart Time Employee is Present");
+                        Console.WriteLine("\nPart Time Employee is Present");
                         empWage = 4;
                         break;
                     default:
-                        Console.WriteLine("Employee is Absent");
+                        Console.WriteLine("\nEmployee is Absent");
                         break;
                 }
                 empWage = empHrs * EMP_RATE_PER_HR;
                 Console.WriteLine("Day {0} Employee Wage:{1}", day, empWage);
                 totalWage += empWage;
+                day++;
+                totalHrs += empWage;
             }
-            Console.WriteLine("Total Employee Wage for {0} days:{1}", (day-1), totalWage);
+            Console.WriteLine("Total Employee Wage for {0} days:{1} and total working hours {2}", (day-1), totalWage, totalHrs);
             Console.ReadLine();
         }
     }
