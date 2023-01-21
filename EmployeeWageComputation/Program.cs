ed24f191eb46a6b48afd5d811c453e8aca655a45
7007singh <shw12021982@gmail.com>
2023-01-21 09:05:06 +0530

    [Shweta] Modified Calculating EmpWage

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -10,21 +10,26 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
-            int isPresent = 1;
-            int empHrs = 0;
-            int empRatePerHr = 20;
+            const int FULL_TIME = 1;
+            const int EMP_RATE_PER_HR = 20;
+            int empHrs = 0, empWage = 0;
+
             Console.WriteLine("Welcome to EmployeeWage Computation");
-            //UC1 - EmployeeAttendance
+            //UC2 - CalculateDailyWage
             Random random = new Random();
             int empAttendance = random.Next(0,2);
-            if(empAttendance == isPresent)
+            if(empAttendance == FULL_TIME)
             {
                 Console.WriteLine("Emplouee is present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
+                //empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HR;
+            Console.WriteLine("Employee Wage:{0}" + empWage);
             Console.ReadLine();
         }
     }
