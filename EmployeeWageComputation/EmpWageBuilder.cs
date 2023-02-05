80ce0157781bad0ec47086ad7402e7cf617229b9
7007singh <shw12021982@gmail.com>
2023-02-05 08:02:40 +0530

    [Shweta]Added: Save the total wage for each company

--- EmployeeWageComputation/EmpWageBuilder.cs
+++ EmployeeWageComputation/EmpWageBuilder.cs
@@ -2,24 +2,32 @@
 
 namespace EmployeeWageComputation
 {
-    internal class Program
+    internal class EmpWageBuilder
     {
         public const int FULL_TIME = 1;
         public const int PART_TIME = 2;
-        /*public const int MAX_WORKING_DAYS = 20;
-        public const int MAX_WORKING_HRS = 100;
-        public const int EMP_RATE_PER_HR = 20;*/
+        public string company;
+        public int maxWorkingDay, maxWorkingHrs, empRatePerHrs;
+
+        public EmpWageBuilder(string company, int maxWorkingDay, int maxWorkingHrs, int empRatePerHrs) 
+        {
+            this.company = company;
+            this.maxWorkingDay = maxWorkingDay;
+            this.maxWorkingHrs = maxWorkingHrs;
+            this.empRatePerHrs = empRatePerHrs;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to EmployeeWage Computation");
-
-            ComputeEmpWage("TCS",25,50,30);
-            ComputeEmpWage("Google", 24, 70, 20);
-            ComputeEmpWage("Mirosoft", 23, 40, 40);
+            EmpWageBuilder tcs = new EmpWageBuilder("TCS", 25, 50, 30);
+            tcs.ComputeEmpWage();
+            EmpWageBuilder google = new EmpWageBuilder("Google", 24, 70, 20);
+            google.ComputeEmpWage();
+            EmpWageBuilder microsoft = new EmpWageBuilder("Mirosoft", 23, 40, 40);
+            microsoft.ComputeEmpWage();
             Console.ReadLine();
         }
-        
-        public static void ComputeEmpWage(string company, int maxWorkingDay, int maxWorkingHrs, int empRatePerHrs)
+        public void ComputeEmpWage()
         {
             int empHrs = 0, empWage = 0, day = 1, totalWage = 0, totalHrs = 0;
 
@@ -30,19 +38,15 @@ namespace EmployeeWageComputation
                 switch (empAttendance)
                 {
                     case FULL_TIME:
-                        //Console.WriteLine("\nFull Time Employee is Present");
                         empHrs = 8;
                         break;
                     case PART_TIME:
-                        //Console.WriteLine("\nPart Time Employee is Present");
                         empWage = 4;
                         break;
                     default:
-                        //Console.WriteLine("\nEmployee is Absent");
                         break;
                 }
                 empWage = empHrs * empRatePerHrs;
-                //Console.WriteLine("Day {0} Employee Wage:{1}", day, empWage);
                 totalWage += empWage;
                 day++;
                 totalHrs += empWage;
